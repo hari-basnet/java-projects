a3cd18d990d04bf4b2090f7f39df20de98df4ef5
Hari Basnet <harikrishnabsnt@gmail.com>
2019-11-05 00:11:05 +0200

    initial commit

---
+++ hello-world/src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = 12;
+
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myLastOne);
+
+
+    }
+}
