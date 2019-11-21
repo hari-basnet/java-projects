4061154f47bd2d2db0e8ffeead030e90686b0929
Hari Basnet <hari.basnet@x-akseli.fi>
2019-11-21 14:50:52 +0200

    descending order for a number

---
+++ DecendingOrder/src/com/java/DescendingOrder.java
@@ -0,0 +1,33 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class DescendingOrder {
+
+	public static int sortDesc(final int num){
+
+		List<String> numAsList = new ArrayList<>();
+
+		// looping through the given number and adding each number to the list
+		for(int i = 0; i < String.valueOf(num).length(); i++){
+			numAsList.add(i, String.valueOf(num).substring(i, i +1));
+		}
+		// using collection methods to reverse the order of the number
+		Collections.sort(numAsList, Collections.reverseOrder());
+
+		String numAsString = "";
+		for (String digit : numAsList){
+			numAsString = numAsString.concat(digit);
+		}
+
+		return Integer.parseInt(numAsString);
+	}
+
+	public static void displaySortedNumber(final int num){
+		System.out.println(sortDesc(num));
+	}
+}
---
+++ DecendingOrder/src/com/java/Main.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    DescendingOrder ddd = new DescendingOrder();
+    ddd.displaySortedNumber(24549848);
+
+    }
+}
