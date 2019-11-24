e6360d277ccb8321fb574a927350b6ca9645f5b6
haribasnet <harikrishnabsnt@gmail.com>
2019-11-24 20:52:30 +0200

    udemy exercises

---
+++ Exercises-java-udemy/src/com/java/Main.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MegaBytesConverter convert = new MegaBytesConverter();
+        convert.printMegaBytesAndKiloBytes(2500);
+
+        //
+        BarkingDog dd = new BarkingDog();
+
+
+    }
+}
