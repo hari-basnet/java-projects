796f4a8fcc49c024ef5dc27c924a3c1905704530
Hari Basnet <hari.basnet@x-akseli.fi>
2019-11-25 18:05:28 +0200

    method overloading example

---
+++ Exercises-java-udemy/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+T
+$src/com/java/LeapYearCalculator.java,a/8/a857377f084a1ad9d31a06c022f21462b9970f52
+L
+src/com/java/BarkingDog.java,7/1/71d0ffd65ea767c6e515dfbb116882d2212de3ef
+T
+$src/com/java/MegaBytesConverter.java,f/6/f6b6789923b21377e64865df712785fd8e746a66
+Q
+!src/com/java/EqualSumChecker.java,f/3/f3a5950d92491a5f583de389c1cd7b8b12b8983c
+S
+#src/com/java/DecimalComparator.java,d/1/d1701ebdefc307b0465345e377b9dfde7d592a15
+F
+src/com/java/Main.java,8/4/84ebedcf04e707fd8e110d7df673fb233c431281
+S
+#src/com/java/TeenNumberChecker.java,4/f/4ff9a296e940dd21bf543c2a46b0e318578c7b69
---
+++ MethodOverloading/src/com/java/Main.java
@@ -0,0 +1,33 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New Score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+
+        // feet and inches
+        FeetAndInchesToCentimeter ft = new FeetAndInchesToCentimeter();
+        ft.calcFeetAndInchesToCentimeters(10,0);
+        ft.calcFeetAndInchesToCentimeters(10);
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("No player name, no player score");
+        return 0;
+    }
+
+}
