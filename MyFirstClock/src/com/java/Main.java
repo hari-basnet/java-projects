0199dde9f87d15d2d62684452a869e1f7e03696f
Hari Basnet <hari.basnet@x-akseli.fi>
2020-06-20 18:42:29 +0300

    part 5 continue

--- MyFirstClock/src/com/java/Main.java
+++ MyFirstClock/src/com/java/Main.java
@@ -4,21 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        ClockHand hour = new ClockHand(24);
-        ClockHand minute = new ClockHand(60);
-        ClockHand seconds = new ClockHand(60);
+        Clock clock = new Clock();
         
         while(true){
-            System.out.println(hour + ":" + minute + ":" + seconds);
-            
-            seconds.advance();
-            if(seconds.getValue() == 60){
-                minute.advance();
-                
-                if(minute.getValue() == 60){
-                    hour.advance();
-                }
-            }
+            System.out.println(clock);
+            clock.advance();
         }
     }
 }
